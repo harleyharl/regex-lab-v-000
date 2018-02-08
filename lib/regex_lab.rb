ce47d1bda2b706644190ecf0091930f293fc8aeb
Harley Rayner <harleymteden@gmail.com>
2018-02-08 20:43:22 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -21,11 +21,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.scan(/\b[A-Z]\w+\b[.,!]/) != nil
-    true
-  else
-    false
-  end
+  !!text.scan(/\b[A-Z]\w+\b[.,!]/) 
 end
 
 def valid_phone_number?(phone)
