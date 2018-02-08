55575b1b08c19e5474ac9e0da1833aecef58060d
Harley Rayner <harleymteden@gmail.com>
2018-02-08 21:10:32 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -29,7 +29,8 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  valid_phone_numbers = phone.scan(/\d{10}/)
+  valid_phone_numbers = []
+  valid_phone_numbers << phone.scan(/\d{10}/)
   valid_phone_numbers
   #  binding.pry
 end
